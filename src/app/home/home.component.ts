7cfe7600a8814165014a852978044fc93804e70a
tyrellshawn <ooluk098@uottawa.ca>
2019-06-17 19:38:32 -0400

    Modified Landing Page Elements.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-home',
@@ -6,12 +16,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  matcher = new MyErrorStateMatcher();
 
   constructor() { }
 
   ngOnInit() {
 
+
   }
 
 }
+
+
+
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
-  MatButtonModule, MatCheckboxModule, MatCardModule, MatGridListModule
+  MatButtonModule, MatCheckboxModule, MatCardModule, MatGridListModule, MatInputModule
 } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule, MatCheckboxModule, MatCardModule,
     MatGridListModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, MatInputModule
   ],
   exports: [
     MatButtonModule, MatCheckboxModule, MatCardModule,
     MatGridListModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, MatInputModule
   ]
 })
 export class MaterialModule { }
