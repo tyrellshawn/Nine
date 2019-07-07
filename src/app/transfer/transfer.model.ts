907ee2b584f9a7d8063bf2411f176d77209d89d8
tyrellshawn <ooluk098@uottawa.ca>
2019-07-07 14:27:41 -0400

    Created Branch For Olas Development. Ran into issues running older version of Node with recent builds.

---
+++ src/app/transfer/transfer.model.ts
@@ -0,0 +1,7 @@
+export class TransferModel {
+  destination_id: string; // Requester Going to
+  source_id: string; // Requester going from
+  amount: number; // The volume of the payment
+  currency_send: string; // The Currency type from the source
+  currency_rec: string; // The Currency on the receiver side
+}
