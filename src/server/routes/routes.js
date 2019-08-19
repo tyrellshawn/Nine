54f227eb32dcf1436c2963368cefffcf0cf6b748
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2019-08-18 20:34:59 -0400

    Add paystack api

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const request = require('request');
-
+const paystack = require('paystack')('secret_key');
 const _ = require('lodash');
 
 const router = express.Router();
 
 // TODO: Get the correct SECRET_KEY
-
+// TODO: Change requests to use current Paystack api
 router.get('resolveBVN', (req, res) => {
     request.get({
         headers: {'Authorization' : 'Bearer SECRET_KEY'},
@@ -32,58 +32,92 @@ router.get('resolveAccountNumber', (req, res) => {
       });
 })
 
-router.post('initiateTransfer', (req, res) => {
-    const body = _.pick(req.body, ['balance', 'reason', 'amount', 'recipient']);
-    request.post({
-            headers: {'Authorization': 'Bearer SECRET_KEY'},
-            url: 'https://api.paystack.co/transfer',
-            body: {'source': body.balance, 'reason': body.reason, 'amount': body.amount, 'recipient': body.recipient},
-        }, (error, response, body) => {
-            if (!!error) {
-                return res.status(response.statusCode).send(`An error occured. ${error}`)
-            }
-            return res.status(response.statusCode).send(response)
+
+/** Paystack API */
+router.post('transaction/initialize', (req, res) => {
+    const body = _.pick(req.body, ['reference', 'amount', 'email', 'plan']);
+
+    paystack.transactions.initialize(body)
+        .then(function(body) {
+            return res.send(body);
+        })
+        .catch(function(error) {
+            return res.send(`An error occured occured. ${error}`);
         });
 });
 
-router.post('chargeAuthorization', (req, res) => {
-    const body = _.pick(req.body, ['authorization_code', 'email', 'amount']);
-    request.post({
-            headers: {'Authorization': 'Bearer SECRET_KEY', 'Content-Type': 'application/json'},
-            url: 'https://api.paystack.co/transfer',
-            body: {'authorization_code': body.authorization_code, 'email': body.email, 'amount': body.amount},
-        }, (error, response, body) => {
-            if (!!error) {
-                return res.status(response.statusCode).send(`An error occured. ${error}`)
-            }
-            return res.status(response.statusCode).send(response)
+router.post('transaction/charge', (req, res) => {
+    const body = _.pick(req.body, ['reference', 'authorization_code', 'email', 'amount']);
+
+    paystack.transactions.charge(body)
+        .then(function(body) {
+            return res.send(body);
+        })
+        .catch(function(error) {
+            return res.send(`An error occured occured. ${error}`);
         });
 });
 
-router.post('createCustomer', (req, res) => {
-    const body = _.pick(req.body, ['email']);
-    request.post({
-            headers: {'Authorization': 'Bearer SECRET_KEY', 'Content-Type': 'application/json'},
-            url: 'https://api.paystack.co/customer',
-            body: {'email': body.email},
-        }, (error, response, body) => {
-            if (!!error) {
-                return res.status(response.statusCode).send(`An error occured. ${error}`)
-            }
-            return res.status(response.statusCode).send(response)
+router.get('/transaction/:id', (req, res) => {
+    const customerId = req.params.id;
+
+    paystack.transactions.initialize(customerId)
+        .then(function(body) {
+            return res.send(body);
+        })
+        .catch(function(error) {
+            return res.send(`An error occured occured. ${error}`);
         });
 });
 
-router.post('listCustomers', (req, res) => {
-    const body = _.pick(req.body, ['email']);
-    request.get({
-            headers: {'Authorization': 'Bearer SECRET_KEY'},
-            url: 'https://api.paystack.co/customer',
-        }, (error, response, body) => {
-            if (!!error) {
-                return res.status(response.statusCode).send(`An error occured. ${error}`)
-            }
-            return res.status(response.statusCode).send(response)
+router.get('/customer/:id', (req, res) => {
+    const customerId = req.params.id;
+
+    paystack.customer.get(customerId)
+        .then(function(body) {
+            return res.send(body);
+        })
+        .catch(function(error) {
+            return res.send(`An error occured occured when customer id ${id}. ${error}`);
+        });
+});
+
+router.patch('/customer/:id', (req, res) => {
+    const body = _.pick(req.body, ['first_name', 'last_name', 'email', 'phone']);
+    const customerId = req.params.id;
+
+    paystack.customer.update(customerId, body)
+        .then(function(body) {
+            return res.send(body);
+        })
+        .catch(function(error) {
+            return res.send(`An error occured occured when customer id ${id}. ${error}`);
+        });
+});
+
+router.get('/customer/list', (req, res) => {
+    paystack.customer.list()
+        .then(function(body) {
+            return res.send(body);
+        })
+        .catch(function(error) {
+            return res.send(`An error occured. ${error}`);
+        });
+});
+
+router.post('/create/customer', (req, res) => {
+    const body = _.pick(req.body, ['email', 'first_name', 'last_name', 'phone']);
+    paystack.customer.create ({
+        email: body.email,
+        first_name: body.first_name,
+        last_name: body.last_name,
+        phone: body.phone
+    })
+        .then(function(body) {
+            return res.send(body);
+        })
+        .catch(function(error) {
+            return res.send(`An error occured while creating customers. ${error}`);
         });
 });
 
